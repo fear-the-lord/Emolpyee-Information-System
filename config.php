eed45ac69f41ebc990afa5812cb24dd51dc068b9
SOUVIK GHOSH <35571958+fear-the-lord@users.noreply.github.com>
2021-01-08 23:26:44 +0530

    Add files via upload

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
     // Configure the database 
    // Development Connection
-    define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'eis');
+    // define('DB_SERVER', 'localhost');
+    // define('DB_USERNAME', 'root');
+    // define('DB_PASSWORD', '');
+    // define('DB_NAME', 'eis');
 
     // Remote Database Connection
-    // define('DB_SERVER', 'remotemysql.com');
-    // define('DB_USERNAME', 'izLyeZOuK8');
-    // define('DB_PASSWORD', 'ZhUV3v342A');
-    // define('DB_NAME', 'izLyeZOuK8');
+    define('DB_SERVER', 'remotemysql.com');
+    define('DB_USERNAME', 'N3xedb91mL');
+    define('DB_PASSWORD', 'JHFi1Bv8nR');
+    define('DB_NAME', 'N3xedb91mL');
 
     // Connect to the database
     $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
