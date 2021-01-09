836df6897a5d85ecca68adb2178553b4e3efab37
SOUVIK GHOSH <35571958+fear-the-lord@users.noreply.github.com>
2021-01-09 12:49:18 +0530

    Add files via upload

--- admin/query6.php
+++ admin/query6.php
@@ -19,7 +19,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="icon" href="employee.png" type="image/icon type">
+    <link rel="icon" href="../employee.png" type="image/icon type">
     <title>Employee Information System</title>
 </head>
 
